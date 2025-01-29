e10a8c569f8072cb03f0734e41b3f716e62f43e0
Brendan Daly <dalysbrendan@gmail.com>
2025-01-29 09:46:20 -0500

    Added CTEs and copying over tables to CSV

--- project_SQL/1_top_paying_jobs_DA_DS.sql
+++ project_SQL/1_top_paying_jobs_DA_DS.sql
@@ -64,4 +64,48 @@ ORDER BY
     salary_year_avg DESC
 
 LIMIT 10;
-    
+
+
+COPY (
+    WITH top_10_data_scientist_jobs AS (
+        SELECT
+            job_id,
+            job_title,
+            job_location,
+            job_schedule_type,
+            salary_year_avg,
+            job_posted_date,
+            company_dim.name AS company_name
+        FROM job_postings_fact
+        LEFT JOIN company_dim
+        ON job_postings_fact.company_id = company_dim.company_id
+        WHERE job_title_short = 'Data Scientist'
+        AND salary_year_avg IS NOT NULL
+        AND job_location ILIKE 'new york%'
+        ORDER BY salary_year_avg DESC
+        LIMIT 10
+    )
+    SELECT * FROM top_10_data_scientist_jobs
+) TO 'C:\Users\dalys\OneDrive\Desktop\SQL_Data_Analysis\project_SQL\top_10_data_science_roles.csv' WITH CSV HEADER;
+
+COPY (
+    WITH top_10_data_analyst_jobs AS (
+        SELECT
+            job_id,
+            job_title,
+            job_location,
+            job_schedule_type,
+            salary_year_avg,
+            job_posted_date,
+            company_dim.name AS company_name
+        FROM job_postings_fact
+        LEFT JOIN company_dim
+        ON job_postings_fact.company_id = company_dim.company_id
+        WHERE job_title_short = 'Data Analyst'
+        AND salary_year_avg IS NOT NULL
+        AND job_location ILIKE 'new york%'
+        ORDER BY salary_year_avg DESC
+        LIMIT 10
+    )
+    SELECT * FROM top_10_data_analyst_jobs
+) TO 'C:\Users\dalys\OneDrive\Desktop\SQL_Data_Analysis\project_SQL\top_10_data_analyst_roles.csv' WITH CSV HEADER;
